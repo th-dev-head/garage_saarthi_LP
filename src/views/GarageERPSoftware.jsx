94970d5cce90d4d83c6ec19f188a31753f403e13
Vatsal Borda <vatsal@techifyhouse.com>
2026-07-23 12:02:45 +0530

    feat: implement and scaffold multiple specialized garage management solution view pages

--- src/views/GarageERPSoftware.jsx
+++ src/views/GarageERPSoftware.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import dynamic from 'next/dynamic';
 import React from "react";
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
